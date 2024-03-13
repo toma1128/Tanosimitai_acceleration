0ca23f9bfee1b1ee4d879fc86970fb8d6707c77b
2230062 <2230062@ecc.ac.jp>
2024-03-14 00:13:30 +0900

    mainにUIを追加しました。

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <math.h>
-#include <cstdlib>
+#include "efont.h"
+#include "efontM5StickCPlus.h"
+#include "efontEnableJa.h"
 
 // peer設定
 esp_now_peer_info_t slave;
-
 
 // 送信コールバック
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
@@ -20,20 +21,19 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("Last Packet Send Status: ");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
+
 // 受信コールバック
-void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
-  char macStr[18];
-  snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-  //確認用メッセージ
-  Serial.printf("Last Packet Recv from: %s\n", macStr);
-  Serial.printf("Last Packet Recv Data(%d): ", data_len);
-  Serial.println("");
-}
+// void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+//   char macStr[18];
+//   snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
+//            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+//   //確認用メッセージ
+//   Serial.printf("Last Packet Recv from: %s\n", macStr);
+//   Serial.printf("Last Packet Recv Data(%d): ", data_len);
+//   Serial.println("");
+// }
 
 TFT_eSprite sprite = TFT_eSprite(&M5.Lcd);  //速度センサーで使う変数
-
-const int M5LED = 10;   //LEDのピン番号
 
 const int M5LED = 10;   //LEDのピン番号
 
@@ -49,6 +49,12 @@ float x, y, z, theta;
 int R1 = 20, R2 = 40, R3 = 60;
 int X0 = 120, Y0 = 67;
 
+char* muscleExplanation[] = {"スクワット","  腕立て","  腹筋"}; //運動を表示するとき使う
+bool uiNum0,uiNum1,uiNum2 = false;                             //一回だけ回ってほしくて回ったらtrueにする(UI表示)
+// UIの変更に使うtrueで初期画面
+bool ui1 = true;    //true 初期画面 false 準備画面
+bool ui2 = true;    //true 準備画面 false 筋トレ画面
+
 int training_pattern;   //筋トレの種類を切り替えるための変数
 int count = 0;          //筋トレした回数をカウントする変数
 bool approval = false;  //部屋を開けるのを承認する変数
@@ -56,41 +62,6 @@ const int GOAL = 10;    //筋トレ達成目標回数
 
 bool minus = false;     //判定がマイナスの時にtrueにする変数
 bool plus = false;      //判定がプラスの時にtrueにする変数
-
-void setup() {
-  M5.begin();
-  M5.IMU.Init();
-  pinMode(M5LED, OUTPUT);     //LEDの初期設定
-  digitalWrite(M5LED, HIGH);  //LEDを消す(念のため)
-
-  M5.Lcd.setRotation(3);
-  Serial.begin(115200);
-
-  // ESP-NOW初期化
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  if (esp_now_init() == ESP_OK) {
-    Serial.println("ESPNow Init Success");
-  } else {
-    Serial.println("ESPNow Init Failed");
-    ESP.restart();
-  }
-  // マルチキャスト用Slave登録
-  memset(&slave, 0, sizeof(slave));
-  for (int i = 0; i < 6; ++i) {
-    slave.peer_addr[i] = (uint8_t)0xff;
-  }
-  esp_err_t addStatus = esp_now_add_peer(&slave);
-  if (addStatus == ESP_OK) {
-    // Pair success
-    Serial.println("Pair success");
-  }
-  // ESP-NOWコールバック登録
-  esp_now_register_send_cb(OnDataSent);
-  esp_now_register_recv_cb(OnDataRecv);
-
-  delay(10);
-}
 
 /**
  * 加速度の値をとって計算するメソッド
@@ -191,11 +162,81 @@ void training(){
   Serial.println();
 }
 
+//初期画面のUI
+void UI0(){
+
+  M5.Lcd.setTextSize(2);
+  
+  printEfont("準備開始",0,60);
+  // 再生ボタン
+  M5.Lcd.fillTriangle(50, 165, 50, 120, 85, 142, WHITE);
+}
+
+//準備画面のUI
+void UI1(){
+  M5.Lcd.setTextSize(1);
+  printEfont("今回の運動は",0,0);
+
+  printEfont(muscleExplanation[training_pattern],30,45);
+  
+  printEfont("   首に付け",0,100);
+  printEfont("  ボタンを押し",0,120);
+  printEfont("  音が鳴るまで",0,140);
+  printEfont("運動してください",0,160);
+  
+  M5.Lcd.setTextSize(2);
+    
+  M5.Lcd.setCursor(38,200);
+  M5.Lcd.println("START");
+}
+
+void setup() {
+  M5.begin();
+  M5.IMU.Init();
+  pinMode(M5LED, OUTPUT);     //LEDの初期設定
+  digitalWrite(M5LED, HIGH);  //LEDを消す(念のため)
+
+  M5.Lcd.setRotation(0);      //画面を縦にする
+  UI0();
+  uiNum0 =! uiNum0;
+  Serial.begin(115200);
+
+  // ESP-NOW初期化
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  } else {
+    Serial.println("ESPNow Init Failed");
+    ESP.restart();
+  }
+  // マルチキャスト用Slave登録
+  memset(&slave, 0, sizeof(slave));
+  for (int i = 0; i < 6; ++i) {
+    slave.peer_addr[i] = (uint8_t)0xff;
+  }
+  esp_err_t addStatus = esp_now_add_peer(&slave);
+  if (addStatus == ESP_OK) {
+    // Pair success
+    Serial.println("Pair success");
+  }
+  // ESP-NOWコールバック登録
+  esp_now_register_send_cb(OnDataSent);
+  //esp_now_register_recv_cb(OnDataRecv);
+
+  delay(10);
+}
+
 void loop() {
   M5.update();
+
   if(M5.BtnA.wasPressed()){ //Aボタンが押されたら計測開始
     digitalWrite(M5LED, HIGH);  //LEDを消す
-    training_pattern = rand() % 3;  //ランダムに筋トレを選択
+    training_pattern = random(3);  //ランダムに筋トレを選択
+    Serial.println(training_pattern);
+    ui1 = !ui1;
+    M5.Lcd.begin();
+    UI1();
 
     while(true){
       measure();  //加速度計測メソッド呼び出し
@@ -213,20 +254,10 @@ void loop() {
         Serial.print("Send Status: ");
         if (result == ESP_OK) { //成功の場合
           Serial.println("Success");
-        }   //↓エラーが起きた場合
-        else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
-          Serial.println("ESPNOW not Init.");
-        } else if (result == ESP_ERR_ESPNOW_ARG) {
-          Serial.println("Invalid Argument");
-        } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
-          Serial.println("Internal Error");
-        } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
-          Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-        } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
-          Serial.println("Peer not found.");
-        } else {
-          Serial.println("Not sure what happened");
         }
+
+        ui1,ui2 = true;                 //UIを初期に戻す
+        uiNum0,uiNum1,uiNum2 = false;   //ui表示メソッドに入る値を初期化
 
         count = 0;                  //回数の初期化
         break;                      //ループから出る
